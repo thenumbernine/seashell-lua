6b0edf5a1803e78acb0adc38ee7c0702d809274a
Christopher Moore <christopher.e.moore@gmail.com>
2023-12-07 18:29:33 +0700

    combined the eqn-generation of the position and normal

--- run.lua
+++ run.lua
@@ -1,6 +1,5 @@
 #!/usr/bin/env luajit
 local table = require 'ext.table'
-local tolua = require 'ext.tolua'
 local fromlua = require 'ext.fromlua'
 local path = require 'ext.path'
 local template = require 'template'
@@ -12,15 +11,19 @@ local GLTex2D = require 'gl.tex2d'
 local GLTexCube = require 'gl.texcube'
 local ig = require 'imgui'
 require 'glapp.view'.useBuiltinMatrixMath = true -- do this before imguiapp.withorbit
-local App = require 'imguiapp.withorbit'()
-local symmath = require 'symmath'
 local ffi = require 'ffi'
 local vec2f = require 'vec-ffi.vec2f'
 local vec3f = require 'vec-ffi.vec3f'
 local vec4f = require 'vec-ffi.vec4f'
 local vector = require 'ffi.cpp.vector'
 local Targets = require 'make.targets'
+
+local App = require 'imguiapp.withorbit'()
+
 App.title = 'seashell'
+
+-- used here and eqn.lua for where to read/write the eqn cache
+App.cachefile = 'cached-eqns.glsl'
 
 function App:initGL(...)
 	App.super.initGL(self, ...)
@@ -64,198 +67,15 @@ function App:initGL(...)
 		fboScaleY = function() self:rebuildFBO() end,
 	}
 
-	local cachefile = 'cached-eqns.lua'
 	Targets{verbose=true, {
-		dsts = {cachefile},
-		srcs = {'run.lua'},		-- cache as long as this file hasn't changed
+		dsts = {self.cachefile},
+		srcs = {'eqn.lua'},		-- cache as long as this file hasn't changed
 		rule = function()
-
-			local vars = self.guivars:map(function(v,k)
-				return symmath.var(k), k
-			end)
-
-			-- chart coordinates
-			local u = symmath.var'u'
-			local v = symmath.var'v'
-
--- give the parameters single-letter names for the html
-local i = 0
-for k,v in pairs(vars) do
-	v:nameForExporter('MathJax', string.char(('a'):byte() + i))
-	i = i + 1
-end
-
-			local exvar = symmath.var'e_x'
-			local ofsvar = symmath.var'\\vec{v}'
-			
-			-- start with our radius ...
-			local x = (
-				-- these should technically combine ... 
-				-- https://www.wolframalpha.com/input?i=exp%28%5B%5By%2C0%2C0%5D%2C+%5B0%2Cy%2C-x%5D%2C%5B0%2Cx%2Cy%5D%5D%29
-				-- but I've broken it in my own matrix-exp, so ...
-				-- :replace() will no longer evaluate this correctly
-				symmath.exp(
-					(vars.shellExpScaleMinV * (1 - v) + vars.shellExpScaleMaxV * v) * symmath.var'I'
-					+ symmath.var'\\star e_y' * 2 * symmath.pi * vars.shellPeriodV * v
-				)
-				* 
-				(
-					-- get a unit circle around origin
-					symmath.exp(
-						symmath.var'\\star e_x' * 2 * symmath.pi * u
-					)
-					* (exvar * 
-						(1
-						-- give the circle profile some oscillations...
-						+ vars.shellPerturbAmplU 
-						* symmath.cos(2 * symmath.pi * vars.shellPerturbPeriodU * u)
-						-- also oscillate along the spiral
-						+ vars.shellPerturbAmplV
-						* symmath.cos(2 * symmath.pi * vars.shellPerturbPeriodV * v)
-					))
-				
-					+ ofsvar
-				)
-			)
-
-			local xorig = x
-			
-			print(x)
-			
-			local ex = symmath.Matrix{1, 0, 0}:T()
-			local ey = symmath.Matrix{0, 1, 0}:T()
-			local ez = symmath.Matrix{0, 0, 1}:T()
-			
-			local Rx = (2 * symmath.pi * u * symmath.Matrix(
-				{0, -1, 0},
-				{1, 0, 0},
-				{0, 0, 0}
-			))():exp()
-			print(Rx)
-
-			-- [[
-			local Rz = (2 * symmath.pi * vars.shellPeriodV * v * symmath.Matrix(
-				{0, 0, 0},
-				{0, 0, -1},
-				{0, 1, 0}
-			))():exp()
-			print(Rz)
-			local zexp = symmath.exp(vars.shellExpScaleMinV * (1 - v) + vars.shellExpScaleMaxV * v)
-			local Rzexp = Rz * zexp
-			--]]
-			--[[ can I combine these into one?
-			-- ... ehhh not at the moment.  matrix-exp doesn't like it.
-			local Rzdiag = vars.shellExpScaleMinV * (1 - v) + vars.shellExpScaleMaxV * v
-			local Rzrot = 2 * symmath.pi * vars.shellPeriodV * v
-			local Rzexp = symmath.Matrix(
-				{Rzdiag , 0, 0},
-				{0, Rzdiag, -Rzrot},
-				{0, Rzrot, Rzdiag}
-			)():exp()
-			print(Rzexp)
-			--]]
-
--- TODO WHY ISNT THIS WORKING?!?!??!??!
---[[			
-			x = x
-				:replace(exvar, ex)
-				:replace(eyvar, ey)
-				:replace(Rxvar, Rx)
-				:replace(Rzvar, Rz)
-				:replace(
-					ofsvar,
-					symmath.Matrix{
-						-- offset in x direction before applying v-based exp rescaling to make pointed spiral shells
-						vars.circleOfsX,
-						-- offset in x by 1 to put the bottom at origin
-						vars.circleOfsY,
-						-- meh
-						vars.circleOfsZ
-					}:T()
-				)
-			x = x()
---]]
--- [[ JUST WRITE IT WITHOUT REPLACE
-			local x = (
-				Rzexp
-				*
-				(
-					-- get a unit circle around origin
-					Rx
-					* (ex * 
-						(1
-						-- give the circle profile some oscillations...
-						+ vars.shellPerturbAmplU
-						* symmath.cos(2 * symmath.pi * vars.shellPerturbPeriodU * u)
-						-- also oscillate along the spiral
-						+ vars.shellPerturbAmplV
-						* symmath.cos(2 * symmath.pi * vars.shellPerturbPeriodV * v)
-					))
-					
-					+ symmath.Matrix{
-						-- offset in y direction before applying v-based exp rescaling to make spiral shells
-						vars.circleOfsX,
-						-- offset it so the bottom is at origin
-						vars.circleOfsY,
-						-- meh
-						vars.circleOfsZ
-					}:T()
-				)
-			)()
---]]
-			path'eqns.html':write(
-				symmath.export.MathJax.header
-				.. symmath.export.MathJax(xorig) .. '<br><br>\n'
-				.. symmath.export.MathJax(x) .. '<br><br>\n'
-				.. symmath.export.MathJax.footer
-			)
-
-			print(x)
-
-			symmath.export.C.numberType = 'float'
-			local poscode = symmath.export.C:toCode{
-				assignOnly = true,
-				output = {
-					{['pos.x'] = x[1][1]},
-					{['pos.y'] = x[2][1]},
-					{['pos.z'] = x[3][1]},
-				},
-				input = {
-					{['vtx.x'] = u},
-					{['vtx.y'] = v},
-				},
-			}
-		print'poscode'
-			print(poscode)
-
-			local df_du = x:diff(u)()
-			local df_dv = x:diff(v)()
-			local n = df_du:T()[1]:cross( df_dv:T()[1] )
-			local normalcode = symmath.export.C:toCode{
-				assignOnly = true,
-				output = {
-					{['normal.x'] = n[1]},
-					{['normal.y'] = n[2]},
-					{['normal.z'] = n[3]},
-				},
-				input = {
-					{['vtx.x'] = u},
-					{['vtx.y'] = v},
-				},
-			}
-		print'normalcode'
-			print(normalcode)
-
-			assert(path(cachefile):write(tolua{
-				poscode=poscode,
-				normalcode=normalcode,
-			}))
+			require 'eqn'(self)
 		end,
-	}}:run(cachefile)
+	}}:run(self.cachefile)
 
-	local d = fromlua((assert(path(cachefile):read())))
-	local poscode = d.poscode
-	local normalcode = d.normalcode
+	local glslcode = assert(path(self.cachefile):read())
 
 	gl.glEnable(gl.GL_DEPTH_TEST)
 	self.bgcolor = vec4f(.3,.3,.3,1)
@@ -276,15 +96,13 @@ uniform mat4 mvMat, projMat;
 
 void main() {
 	vec3 normal;
-	{
-<?=normalcode?>
-	}
-	vec3 normalv = normalize((mvMat * vec4(normal, 0)).xyz);
-
 	vec3 pos;
+	
 	{
-<?=poscode?>
+<?=glslcode?>
 	}
+	
+	vec3 normalv = normalize((mvMat * vec4(normal, 0)).xyz);
 	gl_Position = projMat * (mvMat * vec4(pos, 1.));
 
 	vec3 incident = -mvMat[0].xyz;
@@ -294,8 +112,7 @@ void main() {
 }
 ]], {
 	self = self,
-	poscode = poscode,
-	normalcode = normalcode,
+	glslcode = glslcode,
 }),
 		fragmentCode = [[
 #version 460
